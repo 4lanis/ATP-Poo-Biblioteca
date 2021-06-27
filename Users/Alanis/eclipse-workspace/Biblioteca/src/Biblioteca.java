dcb156f59f0c1b049cb038caf898bb0b8e9c6c7f
Alanis <Alanis@DESKTOP-CM764VQ>
2021-06-27 11:12:32 -0300

    Trabalho prontinho.

---
+++ Users/Alanis/eclipse-workspace/Biblioteca/src/Biblioteca.java
@@ -0,0 +1,47 @@
+import java.io.Serializable;
+
+public abstract class Biblioteca implements Serializable {
+	
+	private String nome;
+	private int anoDePublicacao;
+	private String autor;
+	
+	public Biblioteca(String nome, int anoDePublicacao, String autor) {
+		this.nome = nome;
+		this.anoDePublicacao = anoDePublicacao;
+		this.autor = autor;
+
+}
+	public String toString() {
+		String retorno = "";
+		retorno += "Nome do Livro: " + this.nome + "\n";
+		retorno += "Autor: " + this.autor + "\n";
+		retorno += "Ano de Publicação: " + this.anoDePublicacao + "\n";
+		return retorno;
+	
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getAnoDePublicacao() {
+		return anoDePublicacao;
+	}
+
+	public void setAnoDePublicacao(int anoDePublicacao) {
+		this.anoDePublicacao = anoDePublicacao;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+}
---
+++ Users/Alanis/eclipse-workspace/Biblioteca/src/Romance.java
@@ -0,0 +1,24 @@
+
+public class Romance extends Biblioteca {
+	public String classificacao; 
+
+	public Romance (String nome, int anoDePublicacao, String autor, String subtitulo) {
+		super(nome, anoDePublicacao, autor);
+		this.classificacao = subtitulo;
+		
+	}
+
+	public String getClassificacao() {
+		return classificacao;
+	}
+
+	public void setClassificacao(String subtitulo) {
+		this.classificacao = subtitulo;
+	}
+	public String toString() {
+		String retorno = super.toString();
+		retorno += "Classificação (Histórico, Aventura, Psicológico ou Experimental : " + this.classificacao + "\n";
+		return retorno;
+
+	}
+}
